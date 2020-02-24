d68e34238ad5970229c1391a707262c222bde1ef
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-24 22:32:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,47 @@
+require 'pry'
+
+# def nyc_pigeon_organizer(data)
+#   raw_pigeon_colors = data.values #=>returns all colors
+#   pigeon_names = raw_pigeon_colors.map{|names| names.values}.flatten.uniq
+  
+# # pigeon_names #=> all names listed once in a new array
+#   hash_of_pigeons = {}
+  
+#   hashing_names = pigeon_names.map do |name| 
+#     # hash2 = {}
+#     hash_of_pigeons[name] = {}
+#   binding.pry
+# data.map do |attribute, attribute_type| #value creates empty array after innerkey
+#     hash_of_pigeons[name][key] = []
+#     attribute_type.map do |current_attribute, names_array|
+#     # If current pigeon's name exists inside of the color hash, add the color to pigeons color
+#     # binding.pry
+#     if names_array.include?(name)
+#       hash_of_pigeons[name][attribute_type] << key2.to_s
+#     end
+#     end
+#       end
+#   end
+
+# hash_of_pigeons
+# end
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  raw_pigeon_colors = data.values #=>returns all colors
+  pigeon_names = raw_pigeon_colors.map{|names| names.values}.flatten.uniq
+  
+# pigeon_names #=> all names listed once in a new array
+  hash_of_pigeons = {}
+  
+  hashing_names = pigeon_names.map do |name| 
+    # hash2 = {}
+    hash_of_pigeons[name] = {}
+    data.each do |attribute, color_hash|
+      hash_of_pigeons[name][attribute] = []
+    color_hash.each do |qualities, more_names|
+      hash_of_pigeons[name][attribute] << qualities.to_s if more_names.include?(name)
+        end
+      end
+    end
+    hash_of_pigeons
+  end
